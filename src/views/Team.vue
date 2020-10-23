17263c4a69956f21ef933e2ac28b8efa8046fee5
DanCodes <dan@dancodes.online>
2020-10-23 18:25:47 +0200

    fix spacing

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-12 mx-auto">
+      <div class="col-md-12 px-0">
         <div class="container">
           <div class="row">
             <div
